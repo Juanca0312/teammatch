7d26f7f4c6b3c11cbb2e7cbed09ebe38aac63d85
Juan Carlos Hernández <jchernandez0312@gmail.com>
2021-06-12 18:21:47 -0500

    :sparkles: chat model

--- src/main/java/com/teammatch/model/Player.java
+++ src/main/java/com/teammatch/model/Player.java
@@ -53,6 +53,15 @@ public class Player {
     @JsonIgnore
     List<Game> games;
 
+    //Relationship with Chat
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "players_chats",
+            joinColumns = {@JoinColumn(name = "player_id")},
+            inverseJoinColumns = {@JoinColumn(name = "chat_id")})
+    @JsonIgnore
+    List<Chat> chats;
+
 
     //TODO: MANY TO MANY TEAMS
 
